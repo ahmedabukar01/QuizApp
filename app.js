6f5662bddd93ccc8ecfc5303c662b28ae4fa8016
ahmed abukar <ahmedlearn01@gmail.com>
2020-10-15 14:41:41 +0300

    done

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const number1 = document.querySelector('#num1');
 const number2 = document.querySelector('#num2');
 let submit = document.querySelector('#form');
 let start=document.querySelector('#form1');
-let inputValue = document.querySelector('input[type="text"]');
+let inputValue = document.querySelector('.inputText');
 let scoreNum=0;
 let wrongNum=0;
 let correctNum=0;
+let resultPopup=false;
 let exit=0;
 let oldNum2=0;
 let numOfDataType=0;
 let numOfRandom=500;
+let name='';
 
 function typedata(){
     numOfDataType=Math.floor(Math.random()*4);
+    console.log(numOfDataType);
     switch(numOfDataType){
         case 0:document.querySelector('#type').textContent='+';
                 break;
@@ -45,6 +48,7 @@ function checkLength(x,y){
         }
     }
     else{
+        
         if(num1.length<num2.length){
             number1.innerText+=0;
         }
@@ -59,8 +63,11 @@ function checkLength(x,y){
                     break;
             case 1: switch(num1.length){
                 case 2: number2.style.paddingLeft='13px';
+                        console.log('its two numbers');
                         break;
                 case 3: number2.style.paddingLeft='10px';
+                        console.log('its one number');
+                        break;
             }
         }
 
@@ -71,33 +78,61 @@ function cssStyleClr(){
     number2.style.paddingLeft='0px';
 }
 start.addEventListener('submit',i=>{
+    document.location.reload();
 })
 submit.addEventListener('submit',(x)=>{
     x.preventDefault();
     cssStyleClr();
-    if(exit<6){
+    if(exit<5){
+        console.log(inputValue);
         checkAnswer(inputValue.value);
-        exit++;
+        ++exit;
     }
     else{
+        status();
         finish();
         //document.location.reload();
     }
     
 })
+
+function popup(){
+    let close = document.querySelector('.popup-text button');
+    let popWrapper = document.querySelector('.popup-wrapper');
+    let submit = document.querySelector('.popup-form');
+    let input = document.querySelector('.popup-form input[type="text"]');
+
+    close.addEventListener('click',x=>{
+        popWrapper.style.display='none';
+    })
+
+    submit.addEventListener('submit',x=>{
+        x.preventDefault();
+        name=input.value;
+        input.value='';
+        popWrapper.style.display='none';
+        
+    })
+}
+popup();
+
 function checkAnswer(ans){
     let n1 = number1.innerText;
     let n2 = number2.innerText;
     let correct=0
     switch(numOfDataType){
         case 0: correct = parseInt(n1)+parseInt(n2);
+                console.log(correct);
                 break;
         case 1: correct = parseInt(n1)-parseInt(n2);
+                console.log(correct);
                 break;
         case 2: correct = parseInt(n1)*parseInt(n2);
+                console.log(correct);
                 break;
         case 3: correct = Math.floor(parseInt(n1)/parseInt(n2));
-                        if(!correct){
+                console.log(correct);
+                        if(n2==0){
                             correct='';
                         }
                 break;
@@ -117,7 +152,7 @@ function wrong(){
     document.querySelector('#wrong').innerText=wrongNum;
 }
 function score(){
-    scoreNum+=10;
+    scoreNum+=20;
     correctNum++;
     document.querySelector('#score').innerText=scoreNum;
     document.querySelector('#correct').innerText=correctNum;
@@ -129,7 +164,7 @@ function next(){
 
 }
 function clear(){
-    document.querySelector('input[type="text"]').value='';
+    document.querySelector('#form input[type="text"]').value='';
 }
 function finish(){
     document.querySelector('#num1').style.opacity='0';
@@ -137,6 +172,43 @@ function finish(){
     document.querySelector('#type').style.opacity='0';
     document.querySelector('input[type="text"]').style.readOnly='true';
 }
+
+function status(){
+    if(!resultPopup){
+        resultPopup=true;
+        document.querySelector('.winner-wrapper').style.display='block';
+    let h1 =document.querySelector('.winner-wrapper h1');
+    let feeling = document.querySelector('.feeling');
+    let hisName = document.querySelector('.name');
+    let message = document.querySelector('.message');
+    let hisScore= document.querySelector('#marks');
+    let myClass = document.querySelector('.win-text');
+    let back = document.querySelector('.winner-wrapper a');
+    back.addEventListener('click',x=>{
+        document.querySelector('.winner-wrapper').style.display='none';
+    })
+
+    hisName.innerText=name;
+    hisScore.innerText=scoreNum+'%';
+    if(scoreNum<50){
+        h1.innerText='Lost';
+        feeling.innerText='Sorry !';
+        message.innerText='You failed this Quiz, you have ';
+        myClass.classList='lost-text';
+
+    }
+    else{
+        h1.innerText='Winner';
+        feeling.innerText='Congragulation ';
+        message.innerText='You passed this Quiz With ';
+        myClass.classList='win-text';
+
+    }
+
+    }
+    
+}
+
 display(numOfRandom);
 checkLength(number1,number2);
 
